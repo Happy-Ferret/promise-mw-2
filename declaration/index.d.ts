4e2c94a2803edc78af8b66baefadc641da44f3b2
Willian Krueger <wkrueger128@gmail.com>
2017-03-12 03:11:26 -0300

    manually fix typings

--- declaration/index.d.ts
+++ declaration/index.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="express" />
+/// <reference path="../src/types.d.ts"/>
+
 import express = require('express');
 export import STATUS_CODES = require('./status-codes');
 export declare class Error extends global.Error {
@@ -9,27 +11,39 @@ export declare class Error extends global.Error {
     constructor(httpStatus: STATUS_CODES, message: string);
     constructor(httpStatus: STATUS_CODES, extra: any, message: string);
 }
+
+type keyOfExpress = "header" | "end" | "status" | "sendStatus" | "links" | "send" | "json" | "jsonp"
+    | "sendFile" | "sendfile" | "download" | "contentType" | "type" | "format" | "attachment" | "set"
+    | "headersSent" | "get" | "clearCookie" | "cookie" | "location" | "redirect" | "render" | "locals"
+    | "charset" | "vary" | "app" | "write" | "writeContinue" | "writeHead" | "statusCode" | "statusMessage"
+    | "setHeader" | "setTimeout" | "sendDate" | "getHeader" | "removeHeader" | "addTrailers" | "finished"
+    | "writable" | "addListener" | "emit" | "on" | "once" | "prependListener" | "prependOnceListener"
+    | "removeListener" | "removeAllListeners" | "setMaxListeners" | "getMaxListeners" | "listeners"
+    | "eventNames" | "listenerCount"
+
 export declare const __construct: {
     Error: typeof Error;
     send(...args: any[]): {
-        __call__: "header" | "end" | "status" | "sendStatus" | "links" | "send" | "json" | "jsonp" | "sendFile" | "sendfile" | "download" | "contentType" | "type" | "format" | "attachment" | "set" | "headersSent" | "get" | "clearCookie" | "cookie" | "location" | "redirect" | "render" | "locals" | "charset" | "vary" | "app" | "write" | "writeContinue" | "writeHead" | "statusCode" | "statusMessage" | "setHeader" | "setTimeout" | "sendDate" | "getHeader" | "removeHeader" | "addTrailers" | "finished" | "writable" | "addListener" | "emit" | "on" | "once" | "prependListener" | "prependOnceListener" | "removeListener" | "removeAllListeners" | "setMaxListeners" | "getMaxListeners" | "listeners" | "eventNames" | "listenerCount";
+        __call__: keyofExpress;
         status: STATUS_CODES;
         args: any[];
     };
     render(...args: any[]): PromiseMw2.Command;
-    status(status: STATUS_CODES, call: "header" | "end" | "status" | "sendStatus" | "links" | "send" | "json" | "jsonp" | "sendFile" | "sendfile" | "download" | "contentType" | "type" | "format" | "attachment" | "set" | "headersSent" | "get" | "clearCookie" | "cookie" | "location" | "redirect" | "render" | "locals" | "charset" | "vary" | "app" | "write" | "writeContinue" | "writeHead" | "statusCode" | "statusMessage" | "setHeader" | "setTimeout" | "sendDate" | "getHeader" | "removeHeader" | "addTrailers" | "finished" | "writable" | "addListener" | "emit" | "on" | "once" | "prependListener" | "prependOnceListener" | "removeListener" | "removeAllListeners" | "setMaxListeners" | "getMaxListeners" | "listeners" | "eventNames" | "listenerCount", ...args: any[]): {
-        __call__: "header" | "end" | "status" | "sendStatus" | "links" | "send" | "json" | "jsonp" | "sendFile" | "sendfile" | "download" | "contentType" | "type" | "format" | "attachment" | "set" | "headersSent" | "get" | "clearCookie" | "cookie" | "location" | "redirect" | "render" | "locals" | "charset" | "vary" | "app" | "write" | "writeContinue" | "writeHead" | "statusCode" | "statusMessage" | "setHeader" | "setTimeout" | "sendDate" | "getHeader" | "removeHeader" | "addTrailers" | "finished" | "writable" | "addListener" | "emit" | "on" | "once" | "prependListener" | "prependOnceListener" | "removeListener" | "removeAllListeners" | "setMaxListeners" | "getMaxListeners" | "listeners" | "eventNames" | "listenerCount";
+    status(status: STATUS_CODES, call: keyOfExpress, ...args: any[]): {
+        __call__: keyOfExpress;
         status: STATUS_CODES;
         args: any[];
     };
-    other(call: "header" | "end" | "status" | "sendStatus" | "links" | "send" | "json" | "jsonp" | "sendFile" | "sendfile" | "download" | "contentType" | "type" | "format" | "attachment" | "set" | "headersSent" | "get" | "clearCookie" | "cookie" | "location" | "redirect" | "render" | "locals" | "charset" | "vary" | "app" | "write" | "writeContinue" | "writeHead" | "statusCode" | "statusMessage" | "setHeader" | "setTimeout" | "sendDate" | "getHeader" | "removeHeader" | "addTrailers" | "finished" | "writable" | "addListener" | "emit" | "on" | "once" | "prependListener" | "prependOnceListener" | "removeListener" | "removeAllListeners" | "setMaxListeners" | "getMaxListeners" | "listeners" | "eventNames" | "listenerCount", ...args: any[]): PromiseMw2.Command;
+    other(call: keyOfExpress, ...args: any[]): PromiseMw2.Command;
     next(): PromiseMw2.Command;
 };
 export declare type HookFnResp = void;
 export declare type HookFnT<T extends PromiseMw2.Command> = (cmd: T) => (res: express.Response, next: express.NextFunction) => Promise<HookFnResp>;
 export declare type mwGenerateFnT = (req: express.Request, helper: typeof __construct) => Promise<PromiseMw2.Command>;
 export declare const middlewareFrom: (handler: mwGenerateFnT) => express.RequestHandler;
-export declare type methodsT = 'all' | 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head' | 'checkout' | 'copy' | 'lock' | 'merge' | 'mkactivity' | 'mkcol' | 'move' | 'm-search' | 'notify' | 'purge' | 'report' | 'search' | 'subscribe' | 'trace' | 'unlock' | 'unsubscribe';
+export declare type methodsT = 'all' | 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' |
+    'head' | 'checkout' | 'copy' | 'lock' | 'merge' | 'mkactivity' | 'mkcol' | 'move' | 'm-search'
+    | 'notify' | 'purge' | 'report' | 'search' | 'subscribe' | 'trace' | 'unlock' | 'unsubscribe';
 export declare type pmwMatcher = (path: string, ...handlers: mwGenerateFnT[]) => void;
 export declare type pmwMatcher2 = (...handlers: mwGenerateFnT[]) => void;
 export declare type wrapT = {
